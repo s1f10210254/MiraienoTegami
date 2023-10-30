18d7a670b635774b9ae2a89c4646bd8617b0ddb8
s1f10210254 <s1f10210248@iniad.org>
2023-10-31 02:08:57 +0900

    rennkei

--- utils/url.ts
+++ utils/url.ts
@@ -1,6 +1,6 @@
 const base =()=>{
     if(process.env.NODE_ENV === "production"){
-        return ""
+        return "https://miraieno-tegami.vercel.app/"
     }else if(process.env.NODE_ENV === "development"){
         return ""
     }
